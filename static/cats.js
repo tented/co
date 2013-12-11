c6460fe54ba585a2bd56ae4d8bd8e54f953404ed
tented <pegs@tented.co>
2013-12-11 14:28:33 +0000

    random ad (rallier)

---
+++ static/cats.js
@@ -0,0 +1,43 @@
+//SIDE CAT
+
+//Array containing all the side cats
+
+var theSideImages = new Array()
+theSideImages[0] = '<img src="static/norms-list-cat.png" alt"" title="">'
+theSideImages[1] = '<img src="static/aoc-cat.png" alt"" title="American Outdoor Caming, indoor camping is over-rated">'
+
+//Black magic as far as I am concerned
+var j = 0
+var p = theSideImages.length;
+var preBuffer = new Array()
+for (i = 0; i < p; i++){
+   preBuffer[i] = new Image()
+   preBuffer[i].src = theSideImages[i]
+}
+var whichSideImage = Math.round(Math.random()*(p-1));
+
+function showSideCat(){
+document.write(theSideImages[whichSideImage]);
+}
+
+//HEADER CAT
+
+//Array containing all the header cats
+
+var theHeaderImages = new Array()
+theHeaderImages[0] = '<img src="static/trekitory-cat.png" alt"" title="">'
+theHeaderImages[1] = '<img src="static/campatory-cat.png" alt"" title="">'
+
+//More black magic
+var j = 0
+var p = theHeaderImages.length;
+var preBuffer = new Array()
+for (i = 0; i < p; i++){
+   preBuffer[i] = new Image()
+   preBuffer[i].src = theHeaderImages[i]
+}
+var whichHeaderImage = Math.round(Math.random()*(p-1));
+
+function showHeaderCat(){
+document.write(theHeaderImages[whichHeaderImage]);
+}
