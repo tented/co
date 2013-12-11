ec55fa57a6e09d2aeeda191d67615841a4d37a01
tented <pegs@tented.co>
2013-12-11 15:57:44 +0000

    new ads (rallier)

--- static/cats.js
+++ static/cats.js
@@ -3,8 +3,9 @@
 //Array containing all the side cats
 
 var theSideImages = new Array()
-theSideImages[0] = '<img src="static/norms-list-cat.png" alt"" title="">'
-theSideImages[1] = '<img src="static/aoc-cat.png" alt"" title="American Outdoor Caming, indoor camping is over-rated">'
+theSideImages[0] = '<img src="norms-list-cat.png" alt"" title="">'
+theSideImages[1] = '<img src="aoc-cat.png" alt"" title="American Outdoor Caming, indoor camping is over-rated">'
+theSideImages[1] = '<img src="jacket-aficionado-cat.png" alt"" title="Because the word \'Afficianado\' has not been overused.">'
 
 //Black magic as far as I am concerned
 var j = 0
@@ -25,8 +26,10 @@ document.write(theSideImages[whichSideImage]);
 //Array containing all the header cats
 
 var theHeaderImages = new Array()
-theHeaderImages[0] = '<img src="static/trekitory-cat.png" alt"" title="">'
-theHeaderImages[1] = '<img src="static/campatory-cat.png" alt"" title="">'
+theHeaderImages[0] = '<img src="trekitory-cat.png" alt"" title="">'
+theHeaderImages[1] = '<img src="campatory-cat.png" alt"" title="">'
+theHeaderImages[2] = '<img src="aoc-tents-cat.png" alt"" title="">'
+theHeaderImages[3] = '<img src="campnation-cat.png" alt"" title="">'
 
 //More black magic
 var j = 0
