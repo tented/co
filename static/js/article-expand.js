52881da5f0007c586664f1c71841f34b6a72410d
tented <pegs@tented.co>
2013-12-15 20:49:28 +0000

    updated article expand (rallier)

--- static/js/article-expand.js
+++ static/js/article-expand.js
@@ -2,14 +2,14 @@ function articleExpand(articleId)
 {
 var currentHeight = document.getElementById(articleId).offsetHeight;
 
-if (currentHeight < 300)
+if (currentHeight < 301)
 {
 document.getElementById(articleId).style.maxHeight = '100%';
 document.getElementById("button-"+articleId).innerHTML = "Close";
 }
 else
 {
-document.getElementById(articleId).style.maxHeight = '200px';
+document.getElementById(articleId).style.maxHeight = '300px';
 document.getElementById("button-"+articleId).innerHTML = "Read More";
 }
 
